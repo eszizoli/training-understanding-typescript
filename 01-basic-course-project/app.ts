0e9d3526b1074f1fbb4ddb6bfa949af33dd1cee3
eszizoli <zoltan.eszlari@gmail.com>
2024-12-06 00:24:13 +0100

    enum type

--- 01-basic-course-project/app.ts
+++ 01-basic-course-project/app.ts
@@ -1,17 +1,26 @@
 // const person: {
 //     name: string;
 //     age: number;
+//     hobbies: string[];
+//     role: [number, string]; // this is Tuple type!
 // } = {
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string]; // this is Tuple type!
-} = {
+//     name: 'Maximilian',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+
+const person = {
     name: 'Maximilian',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
 
 // person.role.push('admin');
@@ -26,4 +35,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
     //console.log(hobby.map()); // => ERROR
+}
+
+if (person.role === Role.ADMIN) {
+    console.log('is admin');
 }
